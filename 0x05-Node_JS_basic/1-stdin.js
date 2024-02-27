9d81392f614aec8d5bdd09d9e161323b5345f4d2
Shewanji <marrionwanjiru26@gmail.com>
2024-02-27 13:05:56 +0000

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (data) => {
+  const input = data.trim();
+  if (input) {
+    console.log('Your name is:', input);
+  }
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
