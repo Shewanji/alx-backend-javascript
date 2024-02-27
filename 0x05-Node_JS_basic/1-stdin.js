f033ca63ac9fbaf3a9088536722d6b8c4915cd79
Shewanji <marrionwanjiru26@gmail.com>
2024-02-27 13:16:38 +0000

    code fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,14 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (data) => {
-  const input = data.trim();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
   if (input) {
-    console.log('Your name is:', input);
+    process.stdout.write(`Your name is: ${input}`);
   }
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
